0b38a94b626f6157c7315bdec2e630367dccaf6f
JoWttk <joaowittrock88@gmail.com>
2026-02-23 14:58:17 -0300

    voltei

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
     t.window.title = "Testing"
     t.externalstorage = false
+    t.window.vsync = false
     t.window.width = 1024
     t.window.height = 768
     t.console = true
